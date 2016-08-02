0453635cf00940033ec209a2f8e95039946f3962
madhudskumar <madhudskumar@live.in>
2016-08-02 18:31:45 +0530

    image copy

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,16 @@ var
 var
     source = 'source/',
     dest = 'build/';
+    images = {
+        in: source + 'images/*.*',
+        out: dest + 'images/'
+    }
+
+//manage images
+gulp.task('mani', function () {
+    return gulp.src(images.in)
+        .pipe(gulp.dest(images.out));
+})
 
 //default task
 gulp.task('default', function () {
